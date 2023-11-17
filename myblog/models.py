74dcdc60031985caf5905dfe84cc238c8fe720b7
Егор Дзюблик <149952555+EgorDzyublik@users.noreply.github.com>
2023-11-18 01:00:16 +0300

    Update models.py

--- myblog/models.py
+++ myblog/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from .validators import validate_file_extension
 class Input(models.Model):
     file = models.FileField(upload_to='texts/', validators=[validate_file_extension])
+    new_field = models.CharField(max_length=140, default='SOME STRING')
 
